22d4ef9a39e9f67cb87aacd8b94e33a9f2a83917
Mohamed Feddad <mrf345@gmail.com>
2018-07-12 18:58:59 +0800

    adding source code

---
+++ test.py
@@ -0,0 +1,73 @@
+from unittest import TestCase, main
+from static_parameters import (
+    function_parameters,
+    class_parameters
+)
+
+
+class TestingStaticParameters(TestCase):
+    @function_parameters
+    def toVerify(self, a, b):
+        """ 
+        Simple function to verify function_parameters
+        (a: str) (b: int) 
+        """
+        return a * b
+
+    @function_parameters
+    def toVerifyFalse(self, a, b):
+        """ 
+        Simple function to verify false function_parameters
+        (a: False) (b: False)
+        """
+        return a * b
+    
+    @class_parameters(
+        function_parameters
+    )
+    class toVerifyClass:
+        def toVerify(self, a, b):
+            """
+            Simple function in class to verify 
+            class_parameters ( b : int )
+            ( a:int)
+            """
+            return a + b
+    
+    def test_function_parameters(self):
+        """ to test with the correct input """
+        self.assertEqual(
+            self.toVerify('a', 3), 'aaa')
+
+    def test_class_parameters(self):
+        """ to test with correct input """
+        self.assertEqual(
+            self.toVerifyClass().toVerify(1, 1),
+            2
+        )
+
+    def test_function_parameters_false(self):
+        """ to test with the false input """
+        try:
+            self.toVerify(False, False)
+        except Exception as e:
+            self.assertEqual(type(e), TypeError)
+
+    def test_function_parameters_false_doc(self):
+        try:
+            self.toVerifyFalse(False, False)
+        except Exception as e:
+            self.assertEqual(
+                type(e), TypeError
+            )
+
+    def test_function_parameters_not_callable(self):
+        try:
+            function_parameters(False)
+        except Exception as e:
+            self.assertEqual(
+                type(e), TypeError
+            )
+
+if __name__ == '__main__':
+    main()
