ea42bef2e174bff2fdadc400f4c5ab426f6ae356
mrf3 <mrf345@gmail.com>
2018-07-15 04:34:52 +0800

    fix parameters length bug

--- static_parameters/__init__.py
+++ static_parameters/__init__.py
@@ -1,5 +1,5 @@
 from inspect import (
-    getargspec, getsourcefile, isclass
+    getsourcefile, isclass, signature
 )
 
 def function_parameters(function):
@@ -15,46 +15,48 @@ def function_parameters(function):
             )
         )
     def wrapper(*args, **kwargs):
-        if len(function.__doc__) > 3 and len(
-                args) + len(kwargs) == len(
-                    getargspec(function)[0]):
-            paras_index = [
-                p for p in getargspec(
-                    function
-                )[0] if p not in kwargs.keys()
-            ]
-            for content in [i.split('))')[0
-            ] for i in function.__doc__.replace(
-                ' ', '').split('((') if ':' in i and i.split(
-                    ':')[0] in paras_index or i.split(':'
-                    )[0] in kwargs.keys()]:
-                con = content.split(':')
-                try:
-                    if not isinstance(eval(con[1]), type):
-                        raise TypeError()
-                except Exception:
-                    raise TypeError(
-                        "function_parameters(%s(%s=%s)) " % (
-                            getsourcefile(function) + 
-                            ' ' + function.__name__,
-                            con[0], con[1]
-                        ) + "wrong type given in __doc__" 
-                    )
-                received_arg = kwargs.get(
-                    con[0]) if kwargs.get(
-                            con[0]
-                            ) else args[
-                                paras_index.index(
-                                    con[0])]
-                if type(received_arg) != eval(con[1]):
-                    raise TypeError(
-                        "%s(%s=%s) requires type %s" % (
-                            getsourcefile(function) + 
-                            ' ' + function.__name__,
-                            con[0], type(received_arg),
-                            eval(con[1])
+        if function.__doc__ is not None:
+            if len(function.__doc__) > 3 and len(
+                    args) + len(kwargs) + len(
+                        function.__defaults__ if function.__defaults__ else ''
+                        ) >= len(list(signature(function).parameters)):
+                paras_index = [
+                    p for p in list(signature(
+                        function
+                    ).parameters) if p not in kwargs.keys()
+                ]
+                for content in [i.split('))')[0
+                ] for i in function.__doc__.replace(
+                    ' ', '').split('((') if ':' in i and i.split(
+                        ':')[0] in paras_index or i.split(':'
+                        )[0] in kwargs.keys()]:
+                    con = content.split(':')
+                    try:
+                        if not isinstance(eval(con[1]), type):
+                            raise TypeError()
+                    except Exception:
+                        raise TypeError(
+                            "function_parameters(%s(%s=%s)) " % (
+                                getsourcefile(function) + 
+                                ' ' + function.__name__,
+                                con[0], con[1]
+                            ) + "wrong type given in __doc__" 
                         )
-                    )
+                    received_arg = kwargs.get(
+                        con[0]) if kwargs.get(
+                                con[0]
+                                ) else args[
+                                    paras_index.index(
+                                        con[0])]
+                    if type(received_arg) != eval(con[1]):
+                        raise TypeError(
+                            "%s(%s=%s) requires type %s" % (
+                                getsourcefile(function) + 
+                                ' ' + function.__name__,
+                                con[0], type(received_arg),
+                                eval(con[1])
+                            )
+                        )
         return function(*args, **kwargs)
     return wrapper
 
@@ -80,3 +82,4 @@ def class_parameters(decorator):
                         getattr(the_class, attr)))
         return the_class
     return decorate
+
